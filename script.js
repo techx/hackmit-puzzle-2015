e9cc11ec8ceb37f70a3411df85f082154fe23b4f
kimberli <kimberlizhong@gmail.com>
2015-06-14 03:07:22 -0700

    add bark sound

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 var pic = document.getElementById("picture");
 var song = document.getElementById("song");
+var bark = document.getElementById("bark");
 var playing = false;
+var count = 1;
 
 pic.addEventListener("click",function() {
 	if (!playing) {
 		pic.style.backgroundImage="url('doge2.png')";
-		song.play();
+		var num = Math.random();
+		if (count % 10 == 0) {
+			song.play();
+		}
+		else {
+			bark.play();
+		}
 		playing = true;
+		count += 1;
 	}
 });
 
 song.addEventListener("ended", function() {
+	pic.style.backgroundImage="url('doge1.png')";
+	playing = false;
+})
+
+bark.addEventListener("ended", function() {
 	pic.style.backgroundImage="url('doge1.png')";
 	playing = false;
 })
