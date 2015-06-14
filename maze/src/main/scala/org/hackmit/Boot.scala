84674e5f9fc604ed2b501cfa75a0d38108a101c8
Anish Athalye <me@anishathalye.com>
2015-06-13 21:47:54 -0700

    Make app bind to all interfaces

--- maze/src/main/scala/org/hackmit/Boot.scala
+++ maze/src/main/scala/org/hackmit/Boot.scala
@@ -17,5 +17,5 @@ object Boot extends App {
 
   implicit val timeout = Timeout(5.seconds)
   // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 8080)
 }
