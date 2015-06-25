917bcecacc1011927255e7fd6434140c23498fd7
Kate Yu <katexyu@gmail.com>
2015-06-25 11:36:01 -0700

    Make some of us admins 😏

--- command-center/config.js
+++ command-center/config.js
@@ -19,11 +19,22 @@ config.githubClientId = getOrDie("GITHUB_CLIENT_ID");
 config.githubClientSecret = getOrDie("GITHUB_CLIENT_SECRET");
 config.publicHostUrl = getOrDie("PUBLIC_HOST_URL");
 
+// Github usernames - feel free to add yourself
+config.admins = [ "katexyu",
+                  "anishathalye",
+                  "kimberli",
+                  "vervious",
+                  "Detry322",
+                  "lzhang124",
+                  "ehzhang",
+                  "jenniferjzhang",
+                  "zareenc" ];
+
 config.puzzles = [ Puzzle("url1", function(guess){ return guess == "answer"; }),
                    Puzzle("url2", function(guess){ return guess == "answer"; }),
                    Puzzle("url3", function(guess){ return guess == "answer"; }),
                    Puzzle("url4", function(guess){ return guess == "answer"; }),
                    Puzzle("url5", function(guess){ return guess == "answer"; }),
-                   Puzzle("url6", function(guess){ return guess == "answer"; }) ]
+                   Puzzle("url6", function(guess){ return guess == "answer"; }) ];
 
 module.exports = config;
