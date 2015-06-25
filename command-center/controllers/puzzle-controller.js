ce0f547d4b6b389d4b64aeb5c6edf2bfdf825d05
Kate Yu <katexyu@gmail.com>
2015-06-24 20:31:21 -0700

    Gross puzzle logic 😒 😒 😒 😒 😒

--- command-center/controllers/puzzle-controller.js
+++ command-center/controllers/puzzle-controller.js
@@ -4,11 +4,13 @@ var PuzzleController = {}
 PuzzleController.createNew = function(req, res){
     mongoose.model('PuzzlePart').count({user: req.user._id}, function(err, count){
         if (err){
+            // TODO: Don't send errors back to user
             res.status(500).send(err);
         } else if (count != 0){
             res.status(200).send({})
         } else {
-            mongoose.model('PuzzlePart').create({ user: req.user._id, number: 1, }, function(err, puzzlePart){
+            // TODO: replace with actual first url
+            mongoose.model('PuzzlePart').create({ user: req.user._id, number: 1, url: "https://www.hackmit.org" }, function(err, puzzlePart){
                 if (err){
                     res.status(500).send(err);
                 } else {
