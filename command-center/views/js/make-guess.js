ed83734e94c6a7061c62c0bfee117c9b56d28623
Kate Yu <katexyu@gmail.com>
2015-06-27 12:55:01 -0700

    Fix more bugs and make Edwin happy

--- command-center/views/js/make-guess.js
+++ command-center/views/js/make-guess.js
@@ -10,14 +10,23 @@ window.onload = function(){
     }
 
     var makeGuess = function(){
+        if (!document.forms[0].guess.value.trim()){
+            return;
+        }
         var xmlhttp = new XMLHttpRequest();
         var puzzleNumber = document.forms[0].puzzleNumber.value;
         var guess = document.forms[0].guess.value;
         xmlhttp.open("POST", "/puzzle/guess?puzzleNumber=" + puzzleNumber + "&guess=" + guess, true);
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState === XMLHttpRequest.DONE) {
-                if (xmlhttp.status == 200 || xmlhttp.status == 201) {  
-                    window.location = '/';
+                if (xmlhttp.status == 200 || xmlhttp.status == 201) { 
+                    var correct = JSON.parse(xmlhttp.responseText).correct;
+                    if (correct){
+                        window.location = '/';
+                    } else {
+                        document.getElementById("feedback").innerHTML 
+                        = "I don't think that's the right answer.";
+                    }
                 } else {
                     document.getElementById("error").innerHTML 
                         = JSON.parse(xmlhttp.responseText).error;
