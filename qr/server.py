0e5422017a5d2f3e6218322658cf730e9ea00d77
Fernando Trujano <trujano@mit.edu>
2015-06-28 13:28:46 -0700

    Add server

---
+++ qr/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+import qr
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+
+def hello(name="a"):
+
+    num = str(qr.newPuzzle(name))
+    return render_template('index.html', num=num)
+
+
+
+if __name__ == "__main__":
+    app.run()
