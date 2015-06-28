40952d0bab3408febd1ee0c444c3452b8904c5b6
Anish Athalye <me@anishathalye.com>
2015-06-27 21:55:14 -0700

    Implement validator

--- 6-auth/lib/validator.js
+++ 6-auth/lib/validator.js
@@ -3,7 +3,55 @@ var validator = {};
 /* returns true if password is valid */
 validator.validate = function(password) {
   // return password === 'shibe4life'; // such insecure
-  // TODO actually fill this in; the above comment should stay, it's there on purpose
+
+  // such secure wow:
+
+  function alphabetic(str) {
+    return /^[a-zA-Z()]+$/.test(str);
+  }
+
+  function toBytes(str) {
+    var arr = [];
+    for (var i = 0; i < str.length; i++) {
+      arr.push(str.charCodeAt(i));
+    }
+    return arr;
+  }
+
+  // very confuse
+  function cksum(bytes) {
+    var a = 0;
+    var b = 0;
+    for (var i = 0; i < bytes.length; i++) {
+      a = (a + bytes[i]) % 255;
+      b = (b + a) % 255;
+    }
+    return (b << 8) | a;
+  }
+
+  if (!alphabetic(password)) {
+    // much gross
+    return false;
+  }
+
+  if (password.length < 10) {
+    // very short, much insecure
+    return false;
+  }
+
+  var bytes = toBytes(password);
+  var half = Math.floor(bytes.length / 2);
+  var left = bytes.slice(0, half);
+  var right = bytes.slice(half);
+
+  if (cksum(left) !== 0xd06e) {
+    return false; // much sad, not for doge
+  }
+  if (cksum(right) !== 0xf00d) {
+    return false; // wow unfortunate, not food
+  }
+
+  return true; // very success, 0xd06ef00d
 };
 
 module.exports = validator;
