88c2d722941d24a8a31d3dc42dc9ba7db30e1252
Anish Athalye <me@anishathalye.com>
2015-06-28 16:59:32 -0700

    Add slack invite code

---
+++ 1-slack/invite/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // your community or team name to display on join page.
+  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  // your slack team url (ex: socketio.slack.com)
+  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  // access token of slack
+  // You can generate it in https://api.slack.com/web#auth
+  //
+  // You can test your token via curl:
+  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+  //   --compressed
+  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN'
+};
---
+++ 1-slack/invite/routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('../config');
+
+router.get('/', function(req, res) {
+  res.render('index', { community: config.community });
+});
+
+router.post('/invite', function(req, res) {
+  if (req.body.email) {
+    request.post({
+        url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
+        form: {
+          email: req.body.email,
+          token: config.slacktoken,
+          set_active: true
+        }
+      }, function(err, httpResponse, body) {
+        // body looks like:
+        //   {"ok":true}
+        //       or
+        //   {"ok":false,"error":"already_invited"}
+        if (err) { return res.send('Error:' + err); }
+        body = JSON.parse(body);
+        if (body.ok) {
+          res.send('Success! Check "'+ req.body.email +'" for an invite from Slack.');
+        } else {
+          res.send('Failed! ' + body.error)
+        }
+      });
+  } else {
+    res.status(400).send('email is required.');
+  }
+});
+
+module.exports = router;
