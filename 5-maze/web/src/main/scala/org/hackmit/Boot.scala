ca7bec6e34caca396c900291fa213d96a7fb5a2f
Anish Athalye <me@anishathalye.com>
2015-06-29 19:38:07 -0700

    Add ability to bind to any port

--- 5-maze/web/src/main/scala/org/hackmit/Boot.scala
+++ 5-maze/web/src/main/scala/org/hackmit/Boot.scala
@@ -7,15 +7,20 @@ import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
 
-object Boot extends App {
+object Boot {
 
-  // we need an ActorSystem to host our application in
-  implicit val system = ActorSystem("on-spray-can")
+  def main(args: Array[String]) {
+    val port = args.headOption map (_.toInt) getOrElse 8080
 
-  // create and start our service actor
-  val service = system.actorOf(Props[MazeServiceActor], "maze-service")
+    // we need an ActorSystem to host our application in
+    implicit val system = ActorSystem("on-spray-can")
 
-  implicit val timeout = Timeout(5.seconds)
-  // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 8080)
+    // create and start our service actor
+    val service = system.actorOf(Props[MazeServiceActor], "maze-service")
+
+    implicit val timeout = Timeout(5.seconds)
+    // start a new HTTP server on port 8080 with our service actor as the handler
+    IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = port)
+  }
+
 }
