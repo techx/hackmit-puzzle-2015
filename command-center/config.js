fabdce6527fd0fab60fc48f8187ff3793c798f78
Jennifer Zhang <jenniferjuliazhang@gmail.com>
2015-07-01 00:33:00 -0700

    add the P

--- command-center/config.js
+++ command-center/config.js
@@ -67,7 +67,7 @@ config.puzzles = [
     /* puzzle 2 - origami */
     Puzzle(function(username){ return "http://0x5e1f1ed09e.dogemit.party";}, function(guess){ return guess == "very wow"; }),
     /* puzzle 3 - qr */
-    Puzzle(function(username){ return "http://0xd09eb17e.dogemit.party/" + username;}, checkQR()),
+    Puzzle(function(username){ return "http://0xd09eb17e.dogemit.party/p/" + username;}, checkQR()),
     /* puzzle 4 - audio */
     Puzzle(function(username){ return "http://0xff7d09e.dogemit.party";}, function(guess){ return guess == "such hertz"; }),
     /* puzzle 5 - maze */
