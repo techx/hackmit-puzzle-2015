39657c0e01b7cc04f638b9a9bb7253131a4a0e7e
Kate Yu <katexyu@gmail.com>
2015-07-01 16:57:48 -0700

    Add favicon to CC.

--- command-center/app.js
+++ command-center/app.js
@@ -24,8 +24,7 @@ var passport = require('passport');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/assets/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
